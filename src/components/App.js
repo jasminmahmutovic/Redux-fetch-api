9c9ade992f8a08a6304170b4ce5757acf12d38cb
jasminmahmutovic <jasminm562@gmail.com>
2022-11-10 09:49:34 +0100

    Did a fetch function that fetches data from an API by the help of redux, thunk, axios and lodash

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PostList from "./PostList";
+import "../style/App.css";
+
+const App = () => {
+	return (
+		<div>
+			<PostList />
+		</div>
+	);
+};
+
+export default App;
